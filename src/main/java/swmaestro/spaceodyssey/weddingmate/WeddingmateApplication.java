9db6ec8e7edbdc0d9300443b187f42d5a384833c
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-06-29 21:03:50 +0900

    [SO-26] feat: BaseTimeEntity 설정

--- src/main/java/swmaestro/spaceodyssey/weddingmate/WeddingmateApplication.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/WeddingmateApplication.java
@@ -2,7 +2,9 @@ package swmaestro.spaceodyssey.weddingmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WeddingmateApplication {
 
