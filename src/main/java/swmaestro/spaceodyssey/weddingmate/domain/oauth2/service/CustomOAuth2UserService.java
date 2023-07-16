219d3d286927cae604d1b4e9227367667be755d6
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-07-16 23:30:50 +0900

    [SO-34] feat: access token으로 정보 획득 후 가입
    - loadUser를 override해서 OAuth2UserRequest에 있는 access token으로 유저 정보를 얻는다
    - 획득한 유저 정보를 processOAuth2User()에서 java model과 매핑하고 가입된 경우와 가입하지 않은 경우 구분
    - 결과로 OAuth2User를 구현한 UserPrincipal 객체를 생성

---
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/oauth2/service/CustomOAuth2UserService.java
@@ -0,0 +1,81 @@
+package swmaestro.spaceodyssey.weddingmate.domain.oauth2.service;
+
+import java.util.Optional;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import lombok.RequiredArgsConstructor;
+import swmaestro.spaceodyssey.weddingmate.domain.oauth2.OAuth2UserInfo;
+import swmaestro.spaceodyssey.weddingmate.domain.oauth2.OAuth2UserInfoFactory;
+import swmaestro.spaceodyssey.weddingmate.domain.oauth2.UserPrincipal;
+import swmaestro.spaceodyssey.weddingmate.domain.oauth2.enums.AuthProvider;
+import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
+import swmaestro.spaceodyssey.weddingmate.domain.users.repository.UsersRepository;
+import swmaestro.spaceodyssey.weddingmate.global.exception.oauth2.OAuth2AuthProviderIdNotFoundException;
+import swmaestro.spaceodyssey.weddingmate.global.exception.oauth2.OAuth2DuplicateEmailException;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+	private final UsersRepository usersRepository;
+
+	/* OAuth2UserRequest에 이는 Access Token으로 유저 정보를 가져온다 */
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+		return processOAuth2User(userRequest, oAuth2User);
+	}
+
+	/* 획득한 유저 정보를 Java model과 mapping하고 process 진행 */
+	protected OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+		// 로그인 플랫폼 확인(원래는 enum으로 하지만 우리는 kakao만 함)
+		AuthProvider authProvider = AuthProvider.valueOf(
+			userRequest.getClientRegistration().getRegistrationId().toUpperCase());
+		OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(authProvider,
+			oAuth2User.getAttributes());
+
+		if (!StringUtils.hasLength(oAuth2UserInfo.getOAuth2Id())) {
+			throw new OAuth2AuthProviderIdNotFoundException();
+		}
+
+		Optional<Users> usersOptional = usersRepository.findByAuthProviderId(oAuth2UserInfo.getOAuth2Id());
+		Users user;
+
+		// 이미 해당 이메일을 가진 유저가 존재하는 경우
+		if (usersOptional.isPresent()) {
+			user = usersOptional.get();
+
+			// 가져온 유저의 provider명과 넘어온 provider명이 다른 경우(ex. kakao vs naver)
+			if (!user.getAuthProvider().equals(authProvider)) {
+				// 이미 다른 provider로 가입되어 있기 때문에 가입 불가
+				throw new OAuth2DuplicateEmailException(user.getAuthProvider());
+			}
+			user = updateUser(user, oAuth2UserInfo);
+		} else {
+			user = registerUser(authProvider, oAuth2UserInfo);
+		}
+		return UserPrincipal.create(user, oAuth2UserInfo.getAttributes());
+	}
+
+	private Users registerUser(AuthProvider authProvider, OAuth2UserInfo oAuth2UserInfo) {
+		Users users = Users.builder()
+			.nickname(oAuth2UserInfo.getName())
+			.email(oAuth2UserInfo.getEmail())
+			.imageUrl(oAuth2UserInfo.getImageUrl())
+			.authProvider(authProvider)
+			.authProviderId(oAuth2UserInfo.getOAuth2Id())
+			.build();
+
+		return usersRepository.save(users);
+	}
+
+	private Users updateUser(Users users, OAuth2UserInfo oAuth2UserInfo) {
+		return usersRepository.save(users.updateOAuth2Info(oAuth2UserInfo));
+	}
+}
