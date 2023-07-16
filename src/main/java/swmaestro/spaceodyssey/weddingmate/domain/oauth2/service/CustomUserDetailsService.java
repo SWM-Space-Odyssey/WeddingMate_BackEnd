720a5744d661a2ab5dc14dfc125ee4e9533e2786
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-07-16 23:33:06 +0900

    [SO-34] feat: spring security에서 유저 정보 가져오기

---
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/oauth2/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package swmaestro.spaceodyssey.weddingmate.domain.oauth2.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import swmaestro.spaceodyssey.weddingmate.domain.oauth2.UserPrincipal;
+import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
+import swmaestro.spaceodyssey.weddingmate.domain.users.repository.UsersRepository;
+import swmaestro.spaceodyssey.weddingmate.global.exception.users.UserNameNotFoundException;
+import swmaestro.spaceodyssey.weddingmate.global.exception.users.UserNotFoundException;
+
+// Spring Security에서 유저의 정보를 가져오는 클래스
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private UsersRepository usersRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Users users = usersRepository.findByEmail(email)
+			.orElseThrow(UserNameNotFoundException::new);
+
+		return UserPrincipal.create(users);
+	}
+
+	@Transactional
+	public UserDetails loadUserById(Long id) {
+		Users users = usersRepository.findById(id)
+			.orElseThrow(UserNotFoundException::new);
+
+		return UserPrincipal.create(users);
+	}
+}
