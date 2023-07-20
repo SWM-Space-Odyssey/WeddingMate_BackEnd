b58e18929282fe86a66b2e35a090b966a8dd6696
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-07-20 14:53:46 +0900

    [SO-34] feat: unauthorized 요청 상태코드 수정

--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/exception/oauth2/OAuthUnauthUrlException.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/exception/oauth2/OAuthUnauthUrlException.java
@@ -5,7 +5,7 @@ import static swmaestro.spaceodyssey.weddingmate.global.constant.ResponseConstan
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class OAuthUnauthUrlException extends RuntimeException {
 
 	public OAuthUnauthUrlException() {
