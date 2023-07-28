9c255be060a68ed7b2caa1edf9ca32ddcd4549da
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-07-28 21:02:25 +0900

    [SO-52] fix: 기존 에러 중 잘못된 exception 상속받고 있었던 것들 수정

--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/exception/token/UnAuthorizedRefreshTokenException.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/exception/token/UnAuthorizedRefreshTokenException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnAuthorizedRefreshTokenException extends RuntimeException{
+public class UnAuthorizedRefreshTokenException extends RuntimeException {
 
 	public UnAuthorizedRefreshTokenException() {
 		super(REFRESH_TOKEN_UNAUTHORIZED);
--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/exception/users/UserUnAuthorizedException.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/exception/users/UserUnAuthorizedException.java
@@ -2,12 +2,11 @@ package swmaestro.spaceodyssey.weddingmate.global.exception.users;
 
 import static swmaestro.spaceodyssey.weddingmate.global.constant.ResponseConstant.*;
 
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UserUnAuthorizedException extends ResourceNotFoundException {
+public class UserUnAuthorizedException extends RuntimeException {
 	public UserUnAuthorizedException() {
 		super(USER_UNAUTHORIZED);
 	}
