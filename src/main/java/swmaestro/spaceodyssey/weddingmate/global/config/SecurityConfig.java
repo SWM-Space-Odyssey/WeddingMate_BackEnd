0434e80b676ace8eafa18bc1a63ee1276b4062a9
Yurim Park <63230463+lina1919@users.noreply.github.com>
2023-07-31 14:11:31 +0900

    Update SecurityConfig.java

--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/config/SecurityConfig.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
 		configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트엔드 IP, react만 허용) 핸드폰은 js 요청을 하지 않고 java나 swift 쓰기 때문에 cors에 안 걸림
 		configuration.addAllowedOrigin("http://weddingmate-fe-bucket.s3-website.ap-northeast-2.amazonaws.com");
 		configuration.addAllowedOrigin("http://localhost:5173");
-		configuration.addAllowedOrigin("http://weddingmate.co.kr");
+		configuration.addAllowedOrigin("https://weddingmate.co.kr");
 		configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
 		configuration.setMaxAge(MAX_AGE_SECS);
 
