17dd0ad860e98354406cce94e5e4e2bbfd55b1a7
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-08-08 11:59:45 +0900

    [SO-148] feat: 유저 프로필 API를 공통 응답 형식으로 수정

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/profile/controller/UserProfileController.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/profile/controller/UserProfileController.java
@@ -1,11 +1,11 @@
 package swmaestro.spaceodyssey.weddingmate.domain.profile.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +15,8 @@ import swmaestro.spaceodyssey.weddingmate.domain.profile.dto.PlannerProfileUpdat
 import swmaestro.spaceodyssey.weddingmate.domain.profile.service.ProfileService;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.AuthUsers;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
+import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponse;
+import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponseStatus;
 
 @RestController
 @RequestMapping("/api/v1/profile")
@@ -24,14 +26,22 @@ public class UserProfileController { // profileController가 spring 기본 confi
 	private final ProfileService profileService;
 
 	@GetMapping("/planner")
-	public ResponseEntity<PlannerProfileResDto> getPlannerProfile(@AuthUsers Users users) {
+	@ResponseStatus(HttpStatus.OK)
+	public ApiResponse<Object> getPlannerProfile(@AuthUsers Users users) {
 		PlannerProfileResDto resDto = profileService.getPlannerProfile(users);
-		return new ResponseEntity<>(resDto, HttpStatus.OK);
+		return ApiResponse.builder()
+			.status(ApiResponseStatus.SUCCESS)
+			.data(resDto)
+			.build();
 	}
 
 	@PutMapping("/planner")
-	public ResponseEntity<PlannerProfileUpdateResDto> updatePlannerProfile(@RequestBody PlannerProfileUpdateReqDto reqDto) {
+	@ResponseStatus(HttpStatus.OK)
+	public ApiResponse<Object> updatePlannerProfile(@RequestBody PlannerProfileUpdateReqDto reqDto) {
 		PlannerProfileUpdateResDto resDto = profileService.updatePlannerProfile(reqDto);
-		return new ResponseEntity<>(resDto, HttpStatus.OK);
+		return ApiResponse.builder()
+			.status(ApiResponseStatus.SUCCESS)
+			.data(resDto)
+			.build();
 	}
 }
