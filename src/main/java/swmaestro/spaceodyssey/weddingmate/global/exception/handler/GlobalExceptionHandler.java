f2726e78790c4487d6d3bce6cd29330d9dd0026a
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-08-08 11:58:15 +0900

    [SO-148] feat: globalExceptionHandler의 응답을 공통 응답 형식으로 수정

--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponse;
 import swmaestro.spaceodyssey.weddingmate.global.exception.category.CategoryNotFoundException;
 import swmaestro.spaceodyssey.weddingmate.global.exception.file.FileKakaoProfileDownloadFailureException;
 import swmaestro.spaceodyssey.weddingmate.global.exception.file.FileMalformedUrlException;
@@ -44,126 +45,126 @@ public class GlobalExceptionHandler {
 
 	/*================== Oauth2 Exception ==================*/
 	@ExceptionHandler(OAuth2AuthProviderIdNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handleOAuth2ProviderIdNotFoundException(
+	protected final ApiResponse<Object> handleOAuth2ProviderIdNotFoundException(
 		OAuth2AuthProviderIdNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.OAUTH_PROVIDERID_NOTFOUND, e.getMessage());
 	}
 
 	@ExceptionHandler(OAuth2DuplicateEmailException.class)
-	protected final ResponseEntity<ErrorResponse> handleOAuth2DuplicateEmailException(OAuth2DuplicateEmailException e) {
+	protected final ApiResponse<Object> handleOAuth2DuplicateEmailException(OAuth2DuplicateEmailException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.OAUTH_DUPLICATE_EMAIL, e.getMessage());
 	}
 
 	@ExceptionHandler(OAuthUnauthUrlException.class)
-	protected final ResponseEntity<ErrorResponse> handleOAuthUnauthUrlException(OAuthUnauthUrlException e) {
+	protected final ApiResponse<Object> handleOAuthUnauthUrlException(OAuthUnauthUrlException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.OAUTH_UNAUTHORIZED_URL, e.getMessage());
 	}
 
 	/*================== User Exception ==================*/
 	@ExceptionHandler(UserNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
+	protected final ApiResponse<Object> handleUserNotFoundException(UserNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.USER_NOTFOUND, e.getMessage());
 	}
 
 	@ExceptionHandler(UserNameNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handleUserNameNotFoundException(UserNameNotFoundException e) {
+	protected final ApiResponse<Object> handleUserNameNotFoundException(UserNameNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.USER_NAME_NOTFOUND, e.getMessage());
 	}
 
 	@ExceptionHandler(UserUnAuthorizedException.class)
-	protected final ResponseEntity<ErrorResponse> handleUserUnAuthorizedException(UserUnAuthorizedException e) {
+	protected final ApiResponse<Object> handleUserUnAuthorizedException(UserUnAuthorizedException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.USER_UNAUTHORIZED, e.getMessage());
 	}
 
 	// Planner Exception
 	@ExceptionHandler(PlannerNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handlerPlannerNotFoundException(PlannerNotFoundException e) {
+	protected final ApiResponse<Object> handlerPlannerNotFoundException(PlannerNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.PLANNER_NOTFOUND, e.getMessage());
 	}
 
 	@ExceptionHandler(PlannerDuplicateRegistrationException.class)
-	protected final ResponseEntity<ErrorResponse> handlePlannerDuplicateRegistrationException(
+	protected final ApiResponse<Object> handlePlannerDuplicateRegistrationException(
 		PlannerDuplicateRegistrationException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.PLANNER_DUPLICATE_REGISTRATION, e.getMessage());
 	}
 
 	// Customer Exception
 	@ExceptionHandler(CustomerDuplicateRegistrationException.class)
-	protected final ResponseEntity<ErrorResponse> handleCustomerDuplicateResgistrationException(
+	protected final ApiResponse<Object> handleCustomerDuplicateResgistrationException(
 		CustomerDuplicateRegistrationException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.CUSTOMER_DUPLICATE_REGISTRATION, e.getMessage());
 	}
 
 	/*================== Profile Exception ==================*/
 	@ExceptionHandler(PlannerProfileNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handlerPlannerProfileNotFoundException(
+	protected final ApiResponse<Object> handlerPlannerProfileNotFoundException(
 		PlannerProfileNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.PLANNER_PROFILE_NOTFOUND, e.getMessage());
 	}
 
 	/*================== Token Exception ==================*/
 	@ExceptionHandler(RefreshTokenNotEqualException.class)
-	protected final ResponseEntity<ErrorResponse> handleRefreshTokenNotEqualException(RefreshTokenNotEqualException e) {
+	protected final ApiResponse<Object> handleRefreshTokenNotEqualException(RefreshTokenNotEqualException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.REFRESH_TOKEN_UNAUTHORIZED, e.getMessage());
 	}
 
 	@ExceptionHandler(UnAuthorizedRefreshTokenException.class)
-	protected final ResponseEntity<ErrorResponse> handlerUnAuthorizedRefreshTokenException(
+	protected final ApiResponse<Object> handlerUnAuthorizedRefreshTokenException(
 		UnAuthorizedRefreshTokenException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.REFRESH_TOKEN_NOTFOUND, e.getMessage());
 	}
 
 	/*================== Portfolio Exception ==================*/
 	@ExceptionHandler(PortfolioNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handlePortfolioNotFoundException(PortfolioNotFoundException e) {
+	protected final ApiResponse<Object> handlePortfolioNotFoundException(PortfolioNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.PORTFOLIO_NOTFOUND, e.getMessage());
 	}
 
 	@ExceptionHandler(ItemNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handleItemNotFoundException(ItemNotFoundException e) {
+	protected final ApiResponse<Object> handleItemNotFoundException(ItemNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.ITEM_NOTFOUND, e.getMessage());
 	}
 
 	/*================== Category Exception ==================*/
 	@ExceptionHandler(CategoryNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handleCategoryNotFoundException(CategoryNotFoundException e) {
+	protected final ApiResponse<Object> handleCategoryNotFoundException(CategoryNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.CATEGORY_NOTFOUND, e.getMessage());
 	}
 
 	/*================== File Exception ==================*/
 	@ExceptionHandler(FileNotFoundException.class)
-	protected final ResponseEntity<ErrorResponse> handleFileNotFoundException(FileNotFoundException e) {
+	protected final ApiResponse<Object> handleFileNotFoundException(FileNotFoundException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.FILE_NOTFOUND, e.getMessage());
 	}
 
 	@ExceptionHandler(FileNameEmptyException.class)
-	protected final ResponseEntity<ErrorResponse> handleFileNameEmptyException(FileNameEmptyException e) {
+	protected final ApiResponse<Object> handleFileNameEmptyException(FileNameEmptyException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.FILE_NAME_EMPTY, e.getMessage());
 	}
 
 	@ExceptionHandler(FileUnSupportedExtensionTypeException.class)
-	protected final ResponseEntity<ErrorResponse> handleUnSupportedExtensionTypeException(
+	protected final ApiResponse<Object> handleUnSupportedExtensionTypeException(
 		FileUnSupportedExtensionTypeException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.FILE_UNSUPPORTED_EXTENSION, e.getMessage());
 	}
 
 	@ExceptionHandler(FileNotImageException.class)
-	protected final ResponseEntity<ErrorResponse> handleFileNotImageException(FileNotImageException e) {
+	protected final ApiResponse<Object> handleFileNotImageException(FileNotImageException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.FILE_NOT_IMAGE, e.getMessage());
 	}
 
 	@ExceptionHandler(FileUploadFailureException.class)
-	protected final ResponseEntity<ErrorResponse> handleFileUploadFailureException(FileUploadFailureException e) {
+	protected final ApiResponse<Object> handleFileUploadFailureException(FileUploadFailureException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.FILE_UPLOAD_FAILURE, e.getMessage());
 	}
 
 	@ExceptionHandler(FileMalformedUrlException.class)
-	protected final ResponseEntity<ErrorResponse> handleFileMalformedUrlException(FileMalformedUrlException e) {
+	protected final ApiResponse<Object> handleFileMalformedUrlException(FileMalformedUrlException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.FILE_MALFORMED_URL, e.getMessage());
 	}
 
 	@ExceptionHandler(FileKakaoProfileDownloadFailureException.class)
-	protected final ResponseEntity<ErrorResponse> handleKakaoProfileDownloadFailureException(FileKakaoProfileDownloadFailureException e) {
+	protected final ApiResponse<Object> handleKakaoProfileDownloadFailureException(FileKakaoProfileDownloadFailureException e) {
 		return ErrorResponse.toErrorResponseEntity(ErrorCode.FILE_KAKAO_PROFILE_DOWNLOAD_FAILURE, e.getMessage());
 	}
 }
