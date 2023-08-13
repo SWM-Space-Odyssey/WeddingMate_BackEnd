5946b09c618a88ff9af680b216f47738c41b0f4b
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-08-14 01:22:05 +0900

    [SO-177] feat: portfolio 관련 dto에 @NoArgsConstructor 추가

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/portfolio/dto/PortfolioSaveReqDto.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/portfolio/dto/PortfolioSaveReqDto.java
@@ -1,12 +1,21 @@
 package swmaestro.spaceodyssey.weddingmate.domain.portfolio.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PortfolioSaveReqDto {
 	private String title;
 	private String tags;
 	private String region;
+
+	@Builder
+	public PortfolioSaveReqDto(String title, String tags, String region) {
+		this.title = title;
+		this.tags = tags;
+		this.region = region;
+	}
 }
