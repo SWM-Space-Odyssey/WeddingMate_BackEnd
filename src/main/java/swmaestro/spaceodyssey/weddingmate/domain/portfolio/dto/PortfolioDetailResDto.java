e059203121857b1c65ab9825f8df78ede8caaa59
Yurim Park <63230463+lina1919@users.noreply.github.com>
2023-08-14 18:21:46 +0900

    [SO-184] hotfix : 지역태그 추가 및 DummyEntity에서 UserLike 삭제 (#64)

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/portfolio/dto/PortfolioDetailResDto.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/portfolio/dto/PortfolioDetailResDto.java
@@ -14,17 +14,19 @@ public class PortfolioDetailResDto {
 	private Long id;
 	private String title;
 	private String tagList;
+	private String region;
 	private List<ItemResDto> itemResDtoList;
 	private String repImgUrl;
 	private Boolean isWriter;
 
 	@Builder
-	public PortfolioDetailResDto(Long id, String title, String tagList, List<ItemResDto> itemResDtoList, String repImgUrl, Boolean isWriter) {
+	public PortfolioDetailResDto(Long id, String title, String tagList, List<ItemResDto> itemResDtoList, String repImgUrl, Boolean isWriter, String region) {
 		this.id = id;
 		this.title = title;
 		this.tagList = tagList;
 		this.itemResDtoList = itemResDtoList;
 		this.repImgUrl = repImgUrl;
 		this.isWriter = isWriter;
+		this.region = region;
 	}
 }
