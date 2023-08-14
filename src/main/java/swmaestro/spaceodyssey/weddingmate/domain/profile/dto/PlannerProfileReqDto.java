3616629119f2c8646499b08ae09a2e7cea8b0600
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-08-14 17:26:05 +0900

    [SO-179] feat: 플래너 프로필 req, res dto 형식 수정

---
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/profile/dto/PlannerProfileReqDto.java
@@ -0,0 +1,11 @@
+package swmaestro.spaceodyssey.weddingmate.domain.profile.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlannerProfileReqDto {
+	private Long plannerProfileId;
+}
