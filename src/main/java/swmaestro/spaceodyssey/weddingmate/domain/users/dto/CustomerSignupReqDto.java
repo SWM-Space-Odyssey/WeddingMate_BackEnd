4a229a3f6580bca4648b513e9067100bb321769b
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-08-14 13:25:20 +0900

    [SO-182] feat: 파라미터 객체를 Customer 회원가입 관련 dto, mapper에 적용

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/dto/CustomerSignupReqDto.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/dto/CustomerSignupReqDto.java
@@ -1,16 +1,20 @@
 package swmaestro.spaceodyssey.weddingmate.domain.users.dto;
 
-import java.util.List;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CustomerSignupReqDto {
-	String nickname;
-	String weddingDate;
-	String budget;
-	List<String> portfolioTagList;
-	List<String> plannerTagList;
+	// User 관련 속성
+	private String nickname;
+
+	// Customer 관련 속성
+	private Boolean weddingDateConfirmed;
+	private String weddingDate;
+	private String region;
+	private String budget;
+
+	// CustomerTagList 관련 속성
+	private CustomerTagListDto customerTagList;
 }
--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/mapper/CustomerMapper.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/mapper/CustomerMapper.java
@@ -1,67 +1,28 @@
 package swmaestro.spaceodyssey.weddingmate.domain.users.mapper;
 
-import java.util.List;
-import java.util.Objects;
-
 import org.springframework.stereotype.Component;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import swmaestro.spaceodyssey.weddingmate.domain.category.CategoryEnum;
-import swmaestro.spaceodyssey.weddingmate.domain.category.dto.CategoryMapper;
-import swmaestro.spaceodyssey.weddingmate.domain.category.entity.Category;
-import swmaestro.spaceodyssey.weddingmate.domain.portfolio.mapper.PortfolioTagMapper;
-import swmaestro.spaceodyssey.weddingmate.domain.portfolio.entity.PortfolioTag;
-import swmaestro.spaceodyssey.weddingmate.domain.tag.dto.TagMapper;
-import swmaestro.spaceodyssey.weddingmate.domain.tag.entity.Tag;
 import swmaestro.spaceodyssey.weddingmate.domain.users.dto.CustomerSignupReqDto;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Customer;
-import swmaestro.spaceodyssey.weddingmate.domain.users.entity.PlannerTag;
 
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class CustomerMapper {
-	private final CategoryMapper categoryMapper;
-	private final TagMapper tagMapper;
-	private final PortfolioTagMapper portfolioTagMapper;
-	private final PlannerTagMapper plannerTagMapper;
 
 	public Customer toEntity(CustomerSignupReqDto reqDto) {
-		Customer customer = Customer.builder()
-			.weddingDate(reqDto.getWeddingDate())
+		Customer customer =  Customer.builder()
+			.weddingDateConfirmed(reqDto.getWeddingDateConfirmed())
+			.region(reqDto.getRegion())
 			.budget(reqDto.getBudget())
+			.customerTagList(reqDto.getCustomerTagList())
 			.build();
 
-		List<PortfolioTag> portfolioTagList = getPortfolioTagList(customer, reqDto.getPortfolioTagList());
-		List<PlannerTag> plannerTagList = getPlannerTagList(customer, reqDto.getPlannerTagList());
-
-		customer.createPortfolioTagList(portfolioTagList);
-		customer.createPlannerTagList(plannerTagList);
-
+		if (Boolean.TRUE.equals(reqDto.getWeddingDateConfirmed())) {
+			customer.setWeddingDate(reqDto.getWeddingDate());
+		}
 		return customer;
 	}
-
-	public List<PortfolioTag> getPortfolioTagList(Customer customer, List<String> portfolioTagListReqDtO) {
-		Category portfolioCategory = categoryMapper.findCategoryByContentOrElseCreate(CategoryEnum.PORTFOLIO.name());
-		List<Tag> tagList = portfolioTagListReqDtO
-			.stream().filter(Objects::nonNull)
-			.map(content -> tagMapper.contentToEntity(content, portfolioCategory))
-			.toList();
-		return tagList
-			.stream().filter(Objects::nonNull)
-			.map(tag -> portfolioTagMapper.customerToEntity(customer, tag))
-			.toList();
-	}
-
-	public List<PlannerTag> getPlannerTagList(Customer customer, List<String> plannerTagListReqDto) {
-		Category portfolioCategory = categoryMapper.findCategoryByContentOrElseCreate(CategoryEnum.PLANNER.name());
-		List<Tag> tagList = plannerTagListReqDto
-			.stream().filter(Objects::nonNull)
-			.map(content -> tagMapper.contentToEntity(content, portfolioCategory))
-			.toList();
-		return tagList
-			.stream().filter(Objects::nonNull)
-			.map(tag -> plannerTagMapper.customerToEntity(customer, tag))
-			.toList();
-	}
 }
+
