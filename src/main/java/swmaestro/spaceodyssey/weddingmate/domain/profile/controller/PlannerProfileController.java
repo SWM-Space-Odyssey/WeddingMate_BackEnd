a2401bfc124867bcc0a6cf09b356a586744f435b
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-08-15 16:13:56 +0900

    [SO-179] fix: 플래너 단일 프로필 조회 body -> param으로 수정 및 마이페이지 프로필 API 추가 (#70)
    * [SO-179] feat: UserService에 있던 planner, customer 관련 로직을 PlannerService, CustomerService로 분리
    
    * [SO-179] feat: 플래너 프로필 전체 조회 및 개별 조회 API 생성
    
    * [SO-179] style: 기존 UserProfileController 대신 PlannerProfileController 사용하도록 변경
    
    * [SO-179] feat: 플래너 정보 response에 필요한 중간 dto 생성
    
    * [SO-179] feat: 플래너 프로필 req, res dto 형식 수정
    
    * [SO-179] feat: mapper에 entity를 res dto로 바꿔주는 로직 추가
    
    * [SO-179] feat: profileService에 프로필 전체 조회 및 개별 조회 로직 추가
    
    * [SO-179] feat: plannerProfile, user, planner repository에 프로필 조회 API용 함수 추가
    
    * [SO-179] feat: 분리된 PlannerService와 CustomerService를 회원가입 controller에 반영
    
    * [SO-179] feat: 프로필 업데이트 시 작성자 확인하는 exception 추가
    
    * [SO-179] refactor: Planner에서 PlannerProfile fetch 방식을 EAGER로 수정
    
    * [SO-179] fix: 플래너 단일 조회에서 body 대신 param을 넘기도록 수정
    
    * [SO-179] feat: plannerProfile 조회와 관련된 service 분리
    
    * [SO-179] feat: 마이페이지 플래너 프로필 조회를 위한 req dto 수정
    
    - @AuthUsers로 조회하므로 plannerProfileId 필요없음
    
    * [SO-179] feat: 마이페이지 플래너 프로필 조회용 API 생성
    
    * [SO-179] feat: 마이페이지 플래너 프로필 조회용 로직 추가
    
    * [SO-179] feat: User의 ProfileImage 조회 방식을 LAZY에서 EAGER로 변경
    
    * [SO-179] feat: 플래너임을 명시하기 위한 마이페이지 플래너 API 주소 수정
    
    플래너임을 명시하기 위한
    
    * [SO-179] feat: eager fetch를 활용하도록 수정

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/profile/controller/PlannerProfileController.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/profile/controller/PlannerProfileController.java
@@ -4,20 +4,14 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import swmaestro.spaceodyssey.weddingmate.domain.profile.dto.PlannerProfileReqDto;
 import swmaestro.spaceodyssey.weddingmate.domain.profile.dto.PlannerProfileResDto;
-import swmaestro.spaceodyssey.weddingmate.domain.profile.dto.PlannerProfileUpdateReqDto;
-import swmaestro.spaceodyssey.weddingmate.domain.profile.dto.PlannerProfileUpdateResDto;
-import swmaestro.spaceodyssey.weddingmate.domain.profile.service.ProfileService;
-import swmaestro.spaceodyssey.weddingmate.domain.users.entity.AuthUsers;
-import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
+import swmaestro.spaceodyssey.weddingmate.domain.profile.service.PlannerProfileService;
 import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponse;
 import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponseStatus;
 
@@ -26,36 +20,26 @@ import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponseStatus;
 @RequiredArgsConstructor
 public class PlannerProfileController { // profileController가 spring 기본 config 클래스로 정의되어 있음
 
-	private final ProfileService profileService;
+	private final PlannerProfileService plannerProfileService;
 
 	@GetMapping()
 	@ResponseStatus(HttpStatus.OK)
 	public ApiResponse<Object> getAllPlannerProfileList() {
-		List<PlannerProfileResDto> resDtoList = profileService.getPlannerProfileList();
+		List<PlannerProfileResDto> resDtoList = plannerProfileService.getPlannerProfileList();
 		return ApiResponse.builder()
 			.status(ApiResponseStatus.SUCCESS)
 			.data(resDtoList)
 			.build();
 	}
 
-	@GetMapping("/profile")
+	@GetMapping("/{plannerProfileId}")
 	@ResponseStatus(HttpStatus.OK)
-	public ApiResponse<Object> getPlannerProfileByProfileId(@RequestBody PlannerProfileReqDto plannerProfileReqDto) {
-		PlannerProfileResDto plannerProfileResDto = profileService.getPlannerProfileByProfileId(plannerProfileReqDto);
+	public ApiResponse<Object> getPlannerProfileByProfileId(@PathVariable Long plannerProfileId) {
+		PlannerProfileResDto plannerProfileResDto = plannerProfileService.getPlannerProfileByProfileId(plannerProfileId);
 
 		return ApiResponse.builder()
 			.status(ApiResponseStatus.SUCCESS)
 			.data(plannerProfileResDto)
-			.build();
-	}
-
-	@PutMapping("/profile")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResponse<Object> updatePlannerProfile(@AuthUsers Users users, @RequestBody PlannerProfileUpdateReqDto reqDto) {
-		PlannerProfileUpdateResDto resDto = profileService.updatePlannerProfile(users, reqDto);
-		return ApiResponse.builder()
-			.status(ApiResponseStatus.SUCCESS)
-			.data(resDto)
 			.build();
 	}
 }
