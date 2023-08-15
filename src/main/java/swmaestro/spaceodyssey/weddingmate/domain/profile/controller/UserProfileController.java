8dde3a1bfe37e0886699cf2ad871e38fd9bf4248
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-08-15 15:58:08 +0900

    [SO-179] feat: 마이페이지 플래너 프로필 조회용 API 생성

---
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/profile/controller/UserProfileController.java
@@ -0,0 +1,48 @@
+package swmaestro.spaceodyssey.weddingmate.domain.profile.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import swmaestro.spaceodyssey.weddingmate.domain.profile.dto.PlannerProfileResDto;
+import swmaestro.spaceodyssey.weddingmate.domain.profile.dto.PlannerProfileUpdateReqDto;
+import swmaestro.spaceodyssey.weddingmate.domain.profile.dto.PlannerProfileUpdateResDto;
+import swmaestro.spaceodyssey.weddingmate.domain.profile.service.ProfileService;
+import swmaestro.spaceodyssey.weddingmate.domain.users.entity.AuthUsers;
+import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
+import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponse;
+import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponseStatus;
+
+@RestController
+@RequestMapping("/api/v1/profile")
+@RequiredArgsConstructor
+public class UserProfileController {
+
+	private final ProfileService profileService;
+
+	@GetMapping()
+	@ResponseStatus(HttpStatus.OK)
+	public ApiResponse<Object> getPlannerProfile(@AuthUsers Users users) {
+		PlannerProfileResDto plannerProfileResDto = profileService.getPlannerProfile(users);
+
+		return ApiResponse.builder()
+			.status(ApiResponseStatus.SUCCESS)
+			.data(plannerProfileResDto)
+			.build();
+	}
+
+	@PutMapping()
+	@ResponseStatus(HttpStatus.OK)
+	public ApiResponse<Object> updatePlannerProfile(@AuthUsers Users users, @RequestBody PlannerProfileUpdateReqDto reqDto) {
+		PlannerProfileUpdateResDto resDto = profileService.updatePlannerProfile(users, reqDto);
+		return ApiResponse.builder()
+			.status(ApiResponseStatus.SUCCESS)
+			.data(resDto)
+			.build();
+	}
+}
