1290529b63538a7740dd10d11226c0c33ab50e57
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-09-03 18:37:12 +0900

    [SO-209] feat: ChatRoom, ChatMessage entity 추가

---
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/chat/entity/ChatMessage.java
@@ -0,0 +1,44 @@
+package swmaestro.spaceodyssey.weddingmate.domain.chat.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long chatMessageId;
+
+	private String roomId;
+
+	private String sender;
+
+	private String message;
+
+	private LocalDateTime lastMessageTime;
+
+	@ManyToOne
+	@JoinColumn(name = "roomId", referencedColumnName = "roomId", insertable = false, updatable = false)
+	private ChatRoom chatRoom;
+
+	@Builder
+	public ChatMessage(String sender, String roomId, String message) {
+		this.sender = sender;
+		this.roomId = roomId;
+		this.message = message;
+		this.lastMessageTime = LocalDateTime.now();
+	}
+
+}
