9c614a03ea6a20ee50e1cf4fc43897de5f0c6c54
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-09-07 01:22:11 +0900

    [SO-227] fix: 요청 방식을 HTTPRequest로 변경

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/service/AuthService.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/service/AuthService.java
@@ -4,13 +4,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import swmaestro.spaceodyssey.weddingmate.domain.users.dto.AccessTokenDto;
 import swmaestro.spaceodyssey.weddingmate.global.config.jwt.JwtTokenProvider;
 import swmaestro.spaceodyssey.weddingmate.global.config.redis.RedisService;
+import swmaestro.spaceodyssey.weddingmate.global.exception.token.RefreshTokenCookieNotFoundException;
 import swmaestro.spaceodyssey.weddingmate.global.exception.token.RefreshTokenNotEqualException;
 import swmaestro.spaceodyssey.weddingmate.global.exception.token.UnAuthorizedRefreshTokenException;
+import swmaestro.spaceodyssey.weddingmate.global.utils.CookieUtils;
 
 @Slf4j
 @Service
@@ -18,23 +22,30 @@ import swmaestro.spaceodyssey.weddingmate.global.exception.token.UnAuthorizedRef
 @RequiredArgsConstructor
 public class AuthService {
 
+	private final String COOKIE_KEY = "refreshToken";
 	private final JwtTokenProvider jwtProvider;
 	private final RedisService redisService;
 
-	public AccessTokenDto refresh(String refreshToken) {
+	public AccessTokenDto refresh(HttpServletRequest request) {
+
+		String oldRefreshToken = CookieUtils.getCookie(request, COOKIE_KEY)
+			.map(Cookie::getValue).orElseThrow(RefreshTokenCookieNotFoundException::new);
+
 		// 들어온 refreshToekn 검증
-		if (!jwtProvider.validateToken(refreshToken)) {
+		if (!jwtProvider.validateToken(oldRefreshToken)) {
 			throw new UnAuthorizedRefreshTokenException();
 		}
 		// refreshToken에서 Authentication 추출하기
-		Authentication authentication = jwtProvider.getAuthentication(refreshToken);
+		Authentication authentication = jwtProvider.getAuthentication(oldRefreshToken);
 
 		// Redis의 RefreshToken을 가져오면서, 로그아웃된 사용자인 경우 예외 처리
-		String findRefreshToken = redisService.getRefreshToken(authentication.getName())
+		String savedRefreshToken = redisService.getRefreshToken(authentication.getName())
 			.orElseThrow(() -> new RuntimeException(authentication.getName()));
 
 		// 저장되어있던 refreshToken과 일치하는지 확인
-		if (!refreshToken.equals(findRefreshToken)) {
+		System.out.println(oldRefreshToken);
+		System.out.println(savedRefreshToken);
+		if (!oldRefreshToken.equals(savedRefreshToken)) {
 			throw new RefreshTokenNotEqualException();
 		}
 
