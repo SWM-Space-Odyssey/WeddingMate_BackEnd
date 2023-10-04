71c80c78950f9b1ad547aeaa75564c026d33fe78
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-10-05 01:43:59 +0900

    [SO-141] feat: item, portfolio, customer, planner repository에 soft delete 관련 로직 추가

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/item/repository/ItemsRepository.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/item/repository/ItemsRepository.java
@@ -3,9 +3,10 @@ package swmaestro.spaceodyssey.weddingmate.domain.item.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import swmaestro.spaceodyssey.weddingmate.domain.item.entity.Items;
 
 public interface ItemsRepository extends JpaRepository<Items, Long> {
@@ -16,4 +17,11 @@ public interface ItemsRepository extends JpaRepository<Items, Long> {
 		nativeQuery = true)
 	List<Items> searchItemsByKeyword(@Param("keyword") String keyword);
 
+	@Modifying
+	@Query("UPDATE Items item SET item.isDeleted = true WHERE item IN (SELECT item FROM Portfolios p JOIN p.users u WHERE u.userId = :userId)")
+	void softDeleteItemsByUserId(@Param("userId") Long userId);
+
+	@Modifying
+	@Query("UPDATE Items item SET item.isDeleted = true WHERE item.portfolios.portfolioId = :portfolioId")
+	void softDeleteItemByPortfolioId(@Param("portfolioId") Long portfolioId);
 }
