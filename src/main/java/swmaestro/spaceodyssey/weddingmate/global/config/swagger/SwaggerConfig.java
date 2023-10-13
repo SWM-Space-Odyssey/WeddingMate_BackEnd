6b7d7873fe6ffa9c421370c0d72dc5bfbba742f4
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-10-13 21:56:31 +0900

    [SO-273] fix: server url을 application으로 이동

--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/config/swagger/SwaggerConfig.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/config/swagger/SwaggerConfig.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -30,7 +29,6 @@ public class SwaggerConfig {
 
 		return new OpenAPI()
 			.info(apiInfo())
-			.addServersItem(new Server().url("/"))
 			.addSecurityItem(securityRequirement)
 			.components(components);
 	}
