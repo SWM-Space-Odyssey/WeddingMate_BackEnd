4ce454a5afb51dec4186f7a81639eec114430cdb
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-10-13 18:07:28 +0900

    [SO-273] fix: swagger url 수정 (#124)
    * [SO-273] chore: gradle에 swagger 의존성 추가
    
    * [SO-273] feat: swagger 관련 url securityConfig에서 허용
    
    * [SO-273] chore: swagger 관련 의존성 추가
    
    * [SO-273] chore: swagger 관련 설정 파일 추가
    
    * [SO-273] feat: 각 controller에 swagger 추가
    
    * [SO-273] fix: swagger url 수정

--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/config/SecurityConfig.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
 				.requestMatchers("/actuator/**").permitAll()
 				.requestMatchers("/api/v1/token/**").permitAll()
 				.requestMatchers("/v3/api-docs/**").permitAll()
+				.requestMatchers("/swagger-ui/**").permitAll()
 				.requestMatchers("swagger-ui/**").permitAll()
 				.anyRequest().authenticated()
 		);
