ca56364251b428b7fd83fe94b59de0af58609992
lina1919 <lina1919@naver.com>
2023-10-19 13:57:22 +0900

    [SO-287] refactor : api uri restful하게 변경

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/like/controller/LikesController.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/like/controller/LikesController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+
 import swmaestro.spaceodyssey.weddingmate.domain.like.dto.LikeReqDto;
 import swmaestro.spaceodyssey.weddingmate.domain.like.enums.LikeEnum;
 import swmaestro.spaceodyssey.weddingmate.domain.like.service.LikesService;
@@ -23,9 +25,10 @@ import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
 import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponse;
 import swmaestro.spaceodyssey.weddingmate.global.dto.ApiResponseStatus;
 
+@Tag(name = "Like API", description = "좋아요 관련 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/like")
+@RequestMapping("/api/v1/likes")
 public class LikesController {
 
 
@@ -33,7 +36,7 @@ public class LikesController {
 	private final Map<String, LikesService> likeServiceMap;
 
 	@Operation(summary = "유저가 좋아요한 포트폴리오/아이템/플래너/회사 목록 제공")
-	@GetMapping("/{service}")
+	@GetMapping("/by-user-id/{service}")
 	public ApiResponse<Object> getUserLiked(@AuthUsers Users users, @NotNull @PathVariable("service") LikeEnum service) {
 
 		return ApiResponse.builder()
@@ -43,11 +46,17 @@ public class LikesController {
 	}
 
 	@Operation(summary = "유저의 좋아요 반영(포트폴리오/아이템/플래너/회사)")
-	@PostMapping("")
+	@PostMapping()
 	public ApiResponse<Object> like(@AuthUsers Users users, @RequestBody LikeReqDto likeReqDto) {
-		Boolean isLiked = likesService.like(likeReqDto.getId(), users, likeReqDto.getLikeType());
+		Boolean checkIsLiked = likesService.checkIsLiked(likeReqDto.getId(), users, likeReqDto.getLikeType());
 
-		String resultMessage = Boolean.TRUE.equals(isLiked) ? LIKE_SUCCESS : UNLIKE_SUCCESS;
+		if (Boolean.TRUE.equals(checkIsLiked)) {
+			likesService.unlike(likeReqDto.getId(), users, likeReqDto.getLikeType());
+		} else {
+			likesService.like(likeReqDto.getId(), users, likeReqDto.getLikeType());
+		}
+
+		String resultMessage = Boolean.TRUE.equals(checkIsLiked) ? UNLIKE_SUCCESS : LIKE_SUCCESS;
 
 		return ApiResponse.builder()
 			.status(ApiResponseStatus.SUCCESS)
