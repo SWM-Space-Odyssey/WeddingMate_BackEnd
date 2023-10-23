6b5deb32313ad95aff456a9db86e99b590598820
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-10-23 18:49:42 +0900

    Revert "[SO-298] feat: 카카오 로그인 시 gender와 age 추가 (#133)" (#134)
    This reverts commit 230558553e8807c9c6f466a740896fcf79405fc8.

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,29 +1,29 @@
 package swmaestro.spaceodyssey.weddingmate.domain.oauth2.handler;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import static swmaestro.spaceodyssey.weddingmate.domain.oauth2.service.CookieAuthorizationRequestRepository.*;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import swmaestro.spaceodyssey.weddingmate.domain.oauth2.UserPrincipal;
 import swmaestro.spaceodyssey.weddingmate.domain.oauth2.service.CookieAuthorizationRequestRepository;
-import swmaestro.spaceodyssey.weddingmate.domain.users.enums.UserAccountStatusEnum;
 import swmaestro.spaceodyssey.weddingmate.global.config.jwt.JwtTokenProvider;
 import swmaestro.spaceodyssey.weddingmate.global.config.redis.RedisService;
 import swmaestro.spaceodyssey.weddingmate.global.utils.CookieUtils;
-
-import java.io.IOException;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-
-import static swmaestro.spaceodyssey.weddingmate.domain.oauth2.service.CookieAuthorizationRequestRepository.REDIRECT_URL_PARAM_COOKIE_KEY;
 
 @Slf4j
 @Component
@@ -39,20 +39,13 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-										Authentication authentication) throws IOException, ServletException {
+		Authentication authentication) throws IOException, ServletException {
 
-		UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+		UserPrincipal userPrincipal = (UserPrincipal)authentication.getPrincipal();
 		String email = userPrincipal.getEmail();
-		UserAccountStatusEnum userAccountStatus = userPrincipal.getUsers().getAccountStatus();
 
 		String targetUrl = determineTargetUrl(request, response, authentication);
-		boolean isEligible = checkIsEligible(userAccountStatus);
-		String redirectUrl;
-		if (isEligible) {
-			redirectUrl = makeRedirectUrl(email, targetUrl);
-		} else {
-			redirectUrl = makeNonEligibleRedirectUrl(targetUrl);
-		}
+		String redirectUrl = makeRedirectUrl(email, targetUrl);
 
 		ResponseCookie responseCookie = generateRefreshTokenCookie(email);
 		response.setHeader("Set-Cookie", responseCookie.toString());
@@ -68,27 +61,14 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 		getRedirectStrategy().sendRedirect(request, response, redirectUrl);
 	}
 
-	@Override
 	protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
-										Authentication authentication) {
+		Authentication authentication) {
 		Optional<String> redirectUrl = CookieUtils.getCookie(request, REDIRECT_URL_PARAM_COOKIE_KEY)
-				.map(Cookie::getValue);
+			.map(Cookie::getValue);
 
 		String targetUrl = redirectUrl.orElse(getDefaultTargetUrl());
 		return UriComponentsBuilder.fromUriString(targetUrl)
-				.build().toUriString();
-	}
-
-	private String makeNonEligibleRedirectUrl(String redirectUrl) {
-		if (redirectUrl.equals(getDefaultTargetUrl())) {
-			redirectUrl = loginRedirectUrl;
-		}
-
-		return UriComponentsBuilder.fromHttpUrl(redirectUrl)
-				.path("/oauth2/error")
-				.build()
-				.encode() // percent-endcoding
-				.toUriString();
+			.build().toUriString();
 	}
 
 	private String makeRedirectUrl(String email, String redirectUrl) {
@@ -99,20 +79,20 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 		String accessToken = tokenProvider.createAccessToken(email);
 
 		logger.info(UriComponentsBuilder.fromHttpUrl(redirectUrl)
-				.path("/oauth2/redirect")
-				.queryParam("accessToken", accessToken)
-				.queryParam("redirectUrl", redirectUrl)
-				.build()
-				.encode() // percent-endcoding
-				.toUriString());
+			.path("/oauth2/redirect")
+			.queryParam("accessToken", accessToken)
+			.queryParam("redirectUrl", redirectUrl)
+			.build()
+			.encode() // percent-endcoding
+			.toUriString());
 
 		return UriComponentsBuilder.fromHttpUrl(redirectUrl)
-				.path("/oauth2/redirect")
-				.queryParam("accessToken", accessToken)
-				.queryParam("redirectUrl", redirectUrl)
-				.build()
-				.encode() // percent-endcoding
-				.toUriString();
+			.path("/oauth2/redirect")
+			.queryParam("accessToken", accessToken)
+			.queryParam("redirectUrl", redirectUrl)
+			.build()
+			.encode() // percent-endcoding
+			.toUriString();
 	}
 
 	protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
@@ -129,16 +109,12 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 		redisService.setData("RefreshToken:" + email, refreshToken, refreshTokenValidationTime);
 
 		return ResponseCookie.from("refreshToken", refreshToken)
-				.path("/") // 해당 경로 하위의 페이지에서만 쿠키 접근 허용. 모든 경로에서 접근 허용한다.
-				.domain(".weddingmate.co.kr")
-				.maxAge(TimeUnit.MILLISECONDS.toSeconds(refreshTokenValidationTime)) // 쿠키 만료 시기(초). 없으면 브라우저 닫힐 때 제거
-				.secure(true) // HTTPS로 통신할 때만 쿠키가 전송된다.
-				.sameSite("None") // 크로스 사이트에도 쿠키 전송 가능
-				.httpOnly(true) // JS를 통한 쿠키 접근을 막아, XSS 공격 등을 방어하기 위한 옵션이다.
-				.build();
-	}
-
-	public boolean checkIsEligible(UserAccountStatusEnum userAccountStatus) {
-		return !userAccountStatus.equals(UserAccountStatusEnum.NON_ELIGIBLE);
+			.path("/") // 해당 경로 하위의 페이지에서만 쿠키 접근 허용. 모든 경로에서 접근 허용한다.
+			.domain(".weddingmate.co.kr")
+			.maxAge(TimeUnit.MILLISECONDS.toSeconds(refreshTokenValidationTime)) // 쿠키 만료 시기(초). 없으면 브라우저 닫힐 때 제거
+			.secure(true) // HTTPS로 통신할 때만 쿠키가 전송된다.
+			.sameSite("None") // 크로스 사이트에도 쿠키 전송 가능
+			.httpOnly(true) // JS를 통한 쿠키 접근을 막아, XSS 공격 등을 방어하기 위한 옵션이다.
+			.build();
 	}
 }
