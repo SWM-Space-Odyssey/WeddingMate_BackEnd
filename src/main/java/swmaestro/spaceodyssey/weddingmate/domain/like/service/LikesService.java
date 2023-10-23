011455b04d1b2c2c2f77fd975a4edbce3b2f4c6f
lina1919 <lina1919@naver.com>
2023-10-23 12:40:22 +0900

    [SO-285] refactor : 불필요한 함수 인자 삭제

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/like/service/LikesService.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/like/service/LikesService.java
@@ -6,9 +6,9 @@ import java.util.List;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
 
 public interface LikesService {
-	void increaseLikeCount(String lockName, Long id, boolean isIncrement);
+	void increaseLikeCount(String lockName, Long id);
 
-	void decreaseLikeCount(String lockName, Long id, boolean isIncrement);
+	void decreaseLikeCount(String lockName, Long id);
 
 	<T> List<T> getUserLiked(Users users);
 }
