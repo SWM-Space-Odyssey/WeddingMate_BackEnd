2494569c4669fd5e517f8224ee48d925e0d65267
Yurim Park <63230463+lina1919@users.noreply.github.com>
2023-10-23 16:07:02 +0900

    fix : Order annotation 삭제

--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/config/aop/DistributedLock/DistributedLockAop.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/config/aop/DistributedLock/DistributedLockAop.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Order(1)
 @Aspect
 @Component
 @RequiredArgsConstructor
