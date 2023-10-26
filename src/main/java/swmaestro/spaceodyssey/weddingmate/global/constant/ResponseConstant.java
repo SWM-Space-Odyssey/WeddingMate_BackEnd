676fd66e607502c2923c43155908033320b5213a
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-10-27 02:15:52 +0900

    [SO-128] feat: 정지된 계정으로 로그인했을 경우 정지 날짜를 리턴하도록 변경

--- src/main/java/swmaestro/spaceodyssey/weddingmate/global/constant/ResponseConstant.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/global/constant/ResponseConstant.java
@@ -21,7 +21,7 @@ public class ResponseConstant {
 
 	// accountStatus
 
-	public static final String ACCOUNT_SUSPENDED_MESSAGE = "정지된 계정입니다.";
+	public static final String ACCOUNT_SUSPENDED_MESSAGE = "신고 누적으로 인해 일시 정지된 계정입니다.";
 	public static final String ACCOUNT_WITHDRAW_MESSAGE = "이미 탈퇴한 계정입니다.";
 	public static final String ACCOUNT_BANNED_MESSAGE = "영구 정지된 계정입니다.";
 	public static final String ACCOUNT_NON_ELIGIBLE_MESSAGE = "서비스 대상이 아닌 계정입니다.";
