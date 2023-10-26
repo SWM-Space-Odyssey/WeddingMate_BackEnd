f4d68ea650fdfedd89c44ec77c9ccc7b495e41cc
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-10-27 02:04:06 +0900

    [SO-128] feat: 계정 상태가 SUSPENDED인 user를 조회하는 repository 로직 추가

--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/repository/UsersRepository.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/repository/UsersRepository.java
@@ -1,14 +1,14 @@
 package swmaestro.spaceodyssey.weddingmate.domain.users.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Customers;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Planners;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
 import swmaestro.spaceodyssey.weddingmate.domain.users.enums.UserRegisterStatusEnum;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
 	Optional<Users> findByEmail(String email);
@@ -20,6 +20,9 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
 	Optional<Users> findByAuthProviderId(String authProviderId);
 
 	List<Users> findAllByRegisterStatus(UserRegisterStatusEnum registerStatusEnum);
+
+	@Query("SELECT u FROM Users u WHERE u.accountStatus = 'SUSPENDED'")
+	List<Users> findAllSuspendedUsers();
 
 	Optional<Users> findByNickname(String nickname);
 }
--- src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/service/repositoryservice/UsersRepositoryService.java
+++ src/main/java/swmaestro/spaceodyssey/weddingmate/domain/users/service/repositoryservice/UsersRepositoryService.java
@@ -1,17 +1,16 @@
 package swmaestro.spaceodyssey.weddingmate.domain.users.service.repositoryservice;
 
-import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import lombok.RequiredArgsConstructor;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Customers;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Planners;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
 import swmaestro.spaceodyssey.weddingmate.domain.users.enums.UserRegisterStatusEnum;
 import swmaestro.spaceodyssey.weddingmate.domain.users.repository.UsersRepository;
 import swmaestro.spaceodyssey.weddingmate.global.exception.users.UserNotFoundException;
+
+import java.util.List;
 
 @Transactional
 @Service
@@ -23,25 +22,25 @@ public class UsersRepositoryService {
 	@Transactional(readOnly = true)
 	public Users findUserById(Long id) {
 		return usersRepository.findById(id)
-			.orElseThrow(UserNotFoundException::new);
+				.orElseThrow(UserNotFoundException::new);
 	}
 
 	@Transactional(readOnly = true)
 	public Users findUserByNickname(String nickname) {
 		return usersRepository.findByNickname(nickname)
-			.orElseThrow(UserNotFoundException::new);
+				.orElseThrow(UserNotFoundException::new);
 	}
 
 	@Transactional(readOnly = true)
 	public Users findUserByEmail(String email) {
 		return usersRepository.findByEmail(email)
-			.orElseThrow(UserNotFoundException::new);
+				.orElseThrow(UserNotFoundException::new);
 	}
 
 	@Transactional(readOnly = true)
 	public Users findUserByPlanner(Planners planners) {
 		return usersRepository.findByPlanners(planners)
-			.orElseThrow(UserNotFoundException::new);
+				.orElseThrow(UserNotFoundException::new);
 	}
 
 	@Transactional(readOnly = true)
@@ -52,6 +51,11 @@ public class UsersRepositoryService {
 	@Transactional(readOnly = true)
 	public Users findUserByCustomer(Customers customers) {
 		return usersRepository.findByCustomers(customers)
-			.orElseThrow(UserNotFoundException::new);
+				.orElseThrow(UserNotFoundException::new);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Users> findAllSuspendedUsers() {
+		return usersRepository.findAllSuspendedUsers();
 	}
 }
