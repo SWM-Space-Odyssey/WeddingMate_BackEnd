53f5595673ea4f2e963d1fed2cd8bd8d6f9fb594
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-11-06 22:49:44 +0900

    [SO-303] test: 테스트 실행 후 db에서 조회하여 객체 값 비교하도록 변경

--- src/test/java/swmaestro/spaceodyssey/weddingmate/domain/report/service/UserSuspensionServiceTest.java
+++ src/test/java/swmaestro/spaceodyssey/weddingmate/domain/report/service/UserSuspensionServiceTest.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
+import swmaestro.spaceodyssey.weddingmate.domain.report.repository.ReportRepository;
 import swmaestro.spaceodyssey.weddingmate.domain.users.entity.Users;
 import swmaestro.spaceodyssey.weddingmate.domain.users.enums.UserAccountStatusEnum;
+import swmaestro.spaceodyssey.weddingmate.domain.users.service.repositoryservice.UsersRepositoryService;
 import swmaestro.spaceodyssey.weddingmate.global.config.test.DummyEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 class UserSuspensionServiceTest extends DummyEntity {
@@ -21,8 +25,16 @@ class UserSuspensionServiceTest extends DummyEntity {
 	Users zeroToOneBlockUser;
 	Users oneBlockUser;
 	Users twoBlockUser;
+
 	@InjectMocks
 	UserSuspensionService userSuspensionService;
+
+	@Mock
+	UsersRepositoryService usersRepositoryService;
+
+	@Mock
+	ReportRepository reportRepository;
+
 	private String lockname;
 
 	@BeforeEach
@@ -33,9 +45,13 @@ class UserSuspensionServiceTest extends DummyEntity {
 	@Test
 	@DisplayName("[성공] BLOCK 0, REPORT 0인 유저 신고")
 	void addReportCntToBaseUser() {
-		lockname = REDISSON_LOCK_PREFIX + baseUser.getUserId();
-		Users resultUser = userSuspensionService.addReportCnt(lockname, baseUser.getUserId());
+		// stub
+		doReturn(baseUser).when(usersRepositoryService).findUserById(baseUser.getUserId());
 
+		lockname = REDISSON_LOCK_PREFIX + baseUser.getUserId();
+		userSuspensionService.addReportCnt(lockname, baseUser.getUserId());
+
+		Users resultUser = usersRepositoryService.findUserById(baseUser.getUserId());
 		assertEquals(1, resultUser.getReportCnt());
 		assertEquals(0, resultUser.getBlockCnt());
 		assertEquals(UserAccountStatusEnum.NORMAL, resultUser.getAccountStatus());
@@ -44,9 +60,13 @@ class UserSuspensionServiceTest extends DummyEntity {
 	@Test
 	@DisplayName("[성공] BLOCK 0, REPORT 2인 유저 신고")
 	void addReportCntToZeroToOneBlockUser() {
-		lockname = REDISSON_LOCK_PREFIX + zeroToOneBlockUser.getUserId();
-		Users resultUser = userSuspensionService.addReportCnt(lockname, zeroToOneBlockUser.getUserId());
+		// stub
+		doReturn(zeroToOneBlockUser).when(usersRepositoryService).findUserById(zeroToOneBlockUser.getUserId());
 
+		lockname = REDISSON_LOCK_PREFIX + zeroToOneBlockUser.getUserId();
+		userSuspensionService.addReportCnt(lockname, zeroToOneBlockUser.getUserId());
+
+		Users resultUser = usersRepositoryService.findUserById(zeroToOneBlockUser.getUserId());
 		assertEquals(0, resultUser.getReportCnt());
 		assertEquals(1, resultUser.getBlockCnt());
 		assertEquals(UserAccountStatusEnum.SUSPENDED, resultUser.getAccountStatus());
@@ -55,9 +75,13 @@ class UserSuspensionServiceTest extends DummyEntity {
 	@Test
 	@DisplayName("[성공] BLOCK 1, REPORT 2인 유저 신고")
 	void addReportCntToTwoBlockUser() {
-		lockname = REDISSON_LOCK_PREFIX + oneBlockUser.getUserId();
-		Users resultUser = userSuspensionService.addReportCnt(lockname, oneBlockUser.getUserId());
+		// stub
+		doReturn(oneBlockUser).when(usersRepositoryService).findUserById(oneBlockUser.getUserId());
 
+		lockname = REDISSON_LOCK_PREFIX + oneBlockUser.getUserId();
+		userSuspensionService.addReportCnt(lockname, oneBlockUser.getUserId());
+
+		Users resultUser = usersRepositoryService.findUserById(oneBlockUser.getUserId());
 		assertEquals(0, resultUser.getReportCnt());
 		assertEquals(2, resultUser.getBlockCnt());
 		assertEquals(UserAccountStatusEnum.SUSPENDED, resultUser.getAccountStatus());
@@ -66,9 +90,13 @@ class UserSuspensionServiceTest extends DummyEntity {
 	@Test
 	@DisplayName("[성공] BLOCK 2, REPORT 2인 유저 신고")
 	void addReportCntToThreeBlockUser() {
-		lockname = REDISSON_LOCK_PREFIX + twoBlockUser.getUserId();
-		Users resultUser = userSuspensionService.addReportCnt(lockname, twoBlockUser.getUserId());
+		// stub
+		doReturn(twoBlockUser).when(usersRepositoryService).findUserById(twoBlockUser.getUserId());
 
+		lockname = REDISSON_LOCK_PREFIX + twoBlockUser.getUserId();
+		userSuspensionService.addReportCnt(lockname, twoBlockUser.getUserId());
+
+		Users resultUser = usersRepositoryService.findUserById(twoBlockUser.getUserId());
 		assertEquals(0, resultUser.getReportCnt());
 		assertEquals(3, resultUser.getBlockCnt());
 		assertEquals(UserAccountStatusEnum.BANNED, resultUser.getAccountStatus());
